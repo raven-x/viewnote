b8524649731fd0f565842d9a7f583b94dc76af8c
raven-x <naoseru@gmail.com>
2015-10-03 22:42:36 +0300

    NoteRecordActivity buttons style changed to flat

--- app/src/main/java/com/nr/viewnote/view/MainActivity.java
+++ app/src/main/java/com/nr/viewnote/view/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends RoboGuiceAppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button_default_layer, so long
+        // automatically handle clicks on the Home/Up main_button_default_layer, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
